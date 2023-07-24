86a9b1e503b74b16b4163540208fe13a7a0a6045
Sofia <rybina.sofia@gmail.com>
2023-07-24 15:40:14 +0300

    Вторая цифра 3хзначного числа

---
+++ task010_second_digit_of_num/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine()!);
+int lognum = 0;
+int secnum = 0;
+
+if(100 <= num && num <= 999 || -999 <= num && num <= -100)
+{
+    lognum = num % 100;
+    secnum = lognum / 10;
+    Console.WriteLine($"{num} -> {Math.Abs(secnum)}");
+}
+else
+{
+    Console.WriteLine("Wrong number! Input XXX. Example: 785");
+}
